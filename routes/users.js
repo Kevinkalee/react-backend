fa85e972473d79e61d35c11195a9050c8586fa32
Kevin <kevin.lee.1@ucdconnect.ie>
2018-06-20 22:54:49 +0100

    init

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+
+  res.json([{
+  	id: 1,
+  	username: "Kevin"
+  },{
+  	id: 2, 
+  	username: "Stephanie"
+  }, {
+  	id: 3,
+  	username: "Kelly"
+  }])
+});
+
+module.exports = router;
