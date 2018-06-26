26697d4612b2e9ccedb9bc3b632a1e480fcead83
Kevin <kevin.lee.1@ucdconnect.ie>
2018-06-26 15:08:17 +0100

    Added create table functions

--- routes/db.js
+++ routes/db.js
@@ -21,75 +21,170 @@ function connect_db(connection){
 };
 
 // create database
-function create_database)(connection, name){
+function create_database(connection, name){
 	connection.query('CREATE DATABASE '+ name , function (err,results){
 		if (err) throw err; 
-		console.log('Database '+name ' created!');
+		console.log('Database ' + name ' created!');
 	});	
 }
 
 // creates member table within db
 function create_member_table(connection){
+	
+	connection.query('SELECT 1 FROM Member LIMIT 1', function(err){
+		if (!err){
+			connection.query('CREATE TABLE Member ( \
+				ID INT NOT NULL, \
+				Surname VARCHAR(255), \
+				FirstName VARCHAR(255), \ 
+				Sex ENUM('M','F'), \
+				ContactNumber INT(255), \
+				Address VARCHAR(255), \
+				DOB DATE, \
+				RoleCode VARCHAR(2), \
+				LoCode VARCHAR(3), \
+				CellGroupID VARCHAR(10), \
+				MinistryID VARCHAR(10), \
+		 		Consents VARCHAR(255), \
+				PRIMARY KEY (ID) ) ');
+		} else {
+			console.log("Member table already exists!");
+		}
+	})
 
-	connection.query('CREATE TABLE Member ( \
-		ID INT NOT NULL, \
-		Surname VARCHAR(255), \
-		FirstName VARCHAR(255), \ 
-		Sex ENUM('M','F'), \
-		ContactNumber INT(255), \
-		Address VARCHAR(255), \
-		DOB DATE(), \
-		RoleCode ENUM('P','E','D','C','T','L','M','S','Tr'), \
-		LoCode ENUM('E','A','L'), \
-		CellGroupID ENUM('G100','G200','G500','G300','G700'), \
-		MinistryID ENUM('M1100','M1500'.'M1000','M6000','M7000'), \
- 		Consents VARCHAR(255), \
-		PRIMARY KEY (ID) ) ');
 
 }
 
 // create non members table within db
 function create_non_members_table(connection){
-	
-	connection.query('CREATE TABLE Non_Members( \
-		ID INT NOT NULL, \
-		Surname VARCHAR(255), \ 
-		FirstName VARCHAR(255), \
-		Sex ENUM('M','F'), \
-		ContactNumber INT(255), \
-		Address VARCHAR(255), \
-		Status ENUM('Regular','Visitor'), \
-		PRIMARY KEY(ID)');
+	connection.query('SELECT 1 FROM Non_Members LIMIT 1', function(err){
+		if (!err){
+			connection.query('CREATE TABLE Non_Members( \
+				ID INT NOT NULL, \
+				Surname VARCHAR(255), \ 
+				FirstName VARCHAR(255), \
+				Sex ENUM('M','F'), \
+				ContactNumber INT(255), \
+				Address VARCHAR(255), \
+				Status ENUM('Regular','Visitor'), \
+				PRIMARY KEY(ID)');		
+		} else {
+			console.log("Non Member table already exists!");
+		}
+	})
 }
 
 function create_cell_group_table(connection){
-	
-	connection.query('CREATE TABLE Cell_Group( \
-		ID INT NOT NULL, \
-		GroupID ENUM('G100','G200','G500','G300','G700'), \
-		LeaderID INT, \
-		Venue VARCHAR(255), \
-		Meeting_Day ENUM('MON','TUE','WED','THU','FRI','SAT','SUN'), \
-		Meeting_Time TIME(), \
-		Location_Code ENUM('E','A','L'), \
-		PRIMARY KEY(ID)');
-
+	connection.query('SELECT 1 FROM Cell_Group LIMIT 1', function(err){
+		if (!err){
+			connection.query('CREATE TABLE Cell_Group( \
+				ID INT NOT NULL, \
+				GroupID VARCHAR(10), \
+				LeaderID INT, \
+				Venue VARCHAR(255), \
+				Meeting_Day ENUM('MON','TUE','WED','THU','FRI','SAT','SUN'), \
+				Meeting_Time TIME(), \
+				Location_Code VARCHAR(3), \
+				PRIMARY KEY(ID)');
+		} else {
+			console.log("Cell Group table already exists!");
+		}
+	})
 }
+
 function create_ministry_table(connection){
-
-	connection.query('CREATE TABLE Ministry(\
-		ID INT NOT NULL, \
-		MinistryID VARCHAR(255), \
-		MinistryName VARCHAR(255), \
-		LeaderID VARCHAR(255), \
-		Venue VARCHAR(255), \
-		Meeting_Day ENUM('MON','TUE','WED','THU','FRI','SAT','SUN'), \
-		Meeting_Time TIME(), \
-		Location_Code ENUM('E','A','L'), \
-		PRIMARY KEY(ID)'); 
+	connection.query('SELECT 1 FROM Ministry LIMIT 1', function(err){
+		if(!err){
+			connection.query('CREATE TABLE Ministry(\
+				ID INT NOT NULL, \
+				MinistryID VARCHAR(255), \
+				MinistryName VARCHAR(255), \
+				LeaderID VARCHAR(255), \
+				Venue VARCHAR(255), \
+				Meeting_Day ENUM('MON','TUE','WED','THU','FRI','SAT','SUN'), \
+				Meeting_Time TIME(), \
+				Location_Code VARCHAR(3), \
+				PRIMARY KEY(ID)'); 
+			
+		} else {
+			console.log("Ministry table alread exists!");
+		}
+	})
 }
 
-// TODO Add in functions to create School_Lookup, Location_Lookup, Role_Lookup, Student
+function create_school_lookup_table(connection){
+	connection.query('SELECT 1 FROM School_Lookup LIMIT 1', function(err){
+		if (!err){
+			connection.query('CREATE TABLE School_Lookup(\
+				ID INT NOT NULL, \
+				SchoolCode VARCHAR(3), \ 
+				SchoolName VARCHAR(255) \
+				LeaderID INT, \ 
+				Venue VARCHAR(255), \
+				Meeting_Day ENUM('MON','TUE','WED','THU','FRI','SAT','SUN'), \
+				Meeting_Time TIME(), \
+				Location_Code VARCHAR(3), \
+				PRIMARY KEY(ID)'); 
+		} else {
+			console.log("School_Lookup table already exists!");
+		}
+		
+	})
+}
+
+function create_location_lookup_table(connection){
+	connection.query('SELECT 1 FROM Location_Lookup LIMIT 1', function(err){
+		if (!err){
+
+			connection.query('CREATE TABLE Location_Lookup(\
+				ID INT NOT NULL, \ 
+				Loc_Code VARCHAR(255), \
+				Location VARCHAR(255), \
+				PRIMARY KEY(ID)');
+			
+		} else {
+			console.log("Location_Lookup table already exists!");
+		}
+	})
+}
+
+function create_role_lookup_table(connection){
+	connection.query('SELECT 1 FROM Role_Lookup LIMIT 1', function(err){
+		if (!err){	
+			connection.query('CREATE TABLE Role_Lookup( \
+				ID INT NOT NULL, \
+				Role_Code VARCHAR(255), \
+				Role VARCHAR(255), \
+				PRIMARY KEY(ID)');
+		} else {
+			console.log("Role_Lookup table already exists!");
+		}
+	})
+}
+
+function create_student_table(connection){
+	connection.query('SELECT 1 FROM Student LIMIT 1', function(err){
+		if (!err){
+		connection.query('CREATE TABLE Student( \
+			ID INT NOT NULL, \
+			Student_ID INT, \
+			Surname VARCHAR(255), \
+			FirstName VARCHAR(255), \
+			DOB DATE, \
+			Class VARCHAR(255), \
+			Teacher INT, \
+			SchoolCode VARCHAR(255), \
+			MotherName VARCHAR(255), \
+			FatherName VARCHAR(255), \
+			Is_Christian ENUM('Y','N'), \
+			PRIMARY KEY(ID)');
+		} else {
+		console.log("Student table already exists!");
+		} 	
+	})
+}
+
+
 
 // Selecting and displaying entire table name 
 function query_table(connection , table_name){
