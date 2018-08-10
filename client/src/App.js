6a714784515ea24bba49e7aef727982684ae1703
Kevin <kevin.lee.1@ucdconnect.ie>
2018-08-10 16:08:55 +0100

    Rendering data in table form

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,95 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import logo from './logo.svg';
 import './App.css';
 
+import Table from "./Table"
+// Needed for onTouchTap
+injectTapEventPlugin();
+
 class App extends Component {
   state = {
-    members: []
+    data: [],
   }
-
+  
   componentDidMount(){
     this.getMembers();
   }
 
-  getMembers = _=> {
+  getMembers = _ => {
     fetch('http://localhost:3001/members')
     .then(response =>response.json())
-    .then(response => this.setState({ members: response.data }))
+    .then(response => this.setState({ data: response.data }))
     .catch(err =>console.error(err))
   }
-  renderMember = ({id,FirstName,Surname}) => <div key={id}>{FirstName} {Surname}</div>
+
+  // renderMember = ({id,FirstName,Surname}) => <div key={id}>{FirstName} {Surname}</div>
+
   render() {
-    const {members } = this.state;
+    // const {data} = this.state;
     return (
-      <div className="App">
-        <h1>Church Members</h1>
-        {members.map(this.renderMember)}
+      <MuiThemeProvider>
+        <div className="App">
+          <h1>Church Members</h1>
+  
+        <Table
+          data={this.state.data}
+          header={[
+            {
+              name: "First Name",
+              prop: "FirstName"
+            },
+            {
+              name: "Surname",
+              prop: "Surname"
+            },
+            {
+              name: "Sex",
+              prop: "Sex"
+            },
+            {
+              name: "Contact Number",
+              prop: "ContactNumber"
+            },
+            {
+              name: "Address",
+              prop: "Address"
+            },
+            {
+              name: "DOB",
+              prop: "DOB"
+            },
+            {
+              name: "Role Code",
+              prop: "RoleCode"
+            },
+            {
+              name: "LoCode",
+              prop: "LoCode"
+            },
+            {
+              name: "Cell Group ID",
+              prop: "CellGroupID"
+            },
+            {
+              name: "Ministry ID",
+              prop: "MinistryID"
+            },
+            {
+              name: "Consensts",
+              prop: "Consents"
+            }
+
+
+
+
+          ]}
+          
+          />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
